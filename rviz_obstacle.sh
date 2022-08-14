7f1366bc6ef89899c72fe5b7ad9d90e75ed7afef
raspberrypi <fengrucup@outlook.com>
2022-08-14 05:12:12 +0100

    add scripts

---
+++ rviz_obstacle.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rosrun rviz rviz -d /home/pi/catkin_ws/src/obstacle_detector/resources/obstacle_detector.rviz
---
+++ rviz_wall.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rosrun rviz rviz -d /home/pi/catkin_ws/src/laser_line_extraction/rviz/view.rviz
